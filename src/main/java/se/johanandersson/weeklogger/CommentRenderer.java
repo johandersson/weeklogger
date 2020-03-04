f5c4d59c0ab025e0c493f7b919d47a9902945877
Johan Andersson <johan.andersson@ub.uu.se>
2020-03-04 13:18:44 +0100

    Added tooltip to date format

--- src/main/java/se/johanandersson/weeklogger/CommentRenderer.java
+++ src/main/java/se/johanandersson/weeklogger/CommentRenderer.java
@@ -3,9 +3,8 @@ package se.johanandersson.weeklogger;
 
 import javax.swing.JLabel;
 import javax.swing.JTable;
-import javax.swing.border.Border;
 import javax.swing.table.TableCellRenderer;
-import java.awt.Component;
+import java.awt.*;
 
 public class CommentRenderer extends JLabel
         implements TableCellRenderer {
@@ -21,6 +20,7 @@ public class CommentRenderer extends JLabel
         var comment = (String) table.getModel().getValueAt(row,5);
         setText(comment);
         setToolTipText(comment); //comment
+        this.setFont(this.getFont().deriveFont(Font.PLAIN));
         return this;
     }
 }
--- src/main/java/se/johanandersson/weeklogger/DayRenderer.java
+++ src/main/java/se/johanandersson/weeklogger/DayRenderer.java
@@ -4,9 +4,12 @@ package se.johanandersson.weeklogger;
 import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.util.Locale;
+
 
 public class DayRenderer extends JLabel
         implements TableCellRenderer {
@@ -23,10 +26,13 @@ public class DayRenderer extends JLabel
         setText(date);
         var day = date.split("-");
         LocalDate localDate = LocalDate.of(Integer.valueOf(day[0]), Integer.valueOf(day[1]), Integer.valueOf(day[2]));
+        var formatter = DateTimeFormatter.ofPattern("EEEE, d MMMM");
+        localDate.format(formatter);
         Locale locale=new Locale("sv","SE");
         //Getting the day of week for a given date
-        java.time.DayOfWeek dayOfWeek = localDate.getDayOfWeek();
-        setToolTipText(dayOfWeek.getDisplayName(TextStyle.FULL, locale));
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        setToolTipText(localDate.format(formatter));
+        this.setFont(this.getFont().deriveFont(Font.PLAIN));
         return this;
     }
 }
